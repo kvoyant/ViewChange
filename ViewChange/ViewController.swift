5a907a6f30bfca9b79346863142221394c59484a
kvoyant <kvoyant@gmail.com>
2019-10-01 10:41:26 +0900

    outlet 연결

--- ViewChange/ViewController.swift
+++ ViewChange/ViewController.swift
@@ -15,6 +15,13 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onBtnPresent(_ sender: Any) {
+    }
+    
+    @IBAction func onBtnSeque(_ sender: Any) {
+    }
+    
+    @IBAction func onBtnNevi(_ sender: Any) {
+    }
 }
 
