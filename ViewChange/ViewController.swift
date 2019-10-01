17bfdefef3360b65aa6fc7d78f3f33c2c6a2a44a
kvoyant <kvoyant@gmail.com>
2019-10-01 12:24:27 +0900

    뷰를 이용하는 방법 정리

--- ViewChange/ViewController.swift
+++ ViewChange/ViewController.swift
@@ -32,5 +32,24 @@ class ViewController: UIViewController {
 //
 //        self.navigationController?.pushViewController(newVC, animated: true)
     }
+    
+    //메인뷰가 나타나고 나서 이벤트
+    override func viewDidAppear(_ animated: Bool) {
+        print("viewDidAppear !")
+//        let storyboard:UIStoryboard = self.storyboard!
+//        let newVC:UIViewController = storyboard.instantiateViewController(withIdentifier: "SecondVC")
+//        self.present(newVC, animated: true, completion: nil)
+    }
+    
+    /*
+     뷰를 이용하는 방법 (일반적인 방법 3가지)
+     1. present 방법 : 모달뷰( 화면위 중첩)
+     2. seque를 이용하는 방법
+     3. navigationController를 이용하는 방법
+     
+     (특별한 방법)
+     4. TabBarController 를 이용
+     5. 뷰페이저를 이용
+     */
 }
 
